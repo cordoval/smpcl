b44dae61b01872ea55ecc4cf24b7905f583665c7
Germanaz0 <germanazo@gmail.com>
2012-01-13 01:36:33 -0300

    Se modifico el CRUD del Classifield Entity Se modifico la entidad de Clasificados Se mejoraron los routings

--- src/smpcl/ClassifieldBundle/Form/ClassifieldType.php
+++ src/smpcl/ClassifieldBundle/Form/ClassifieldType.php
@@ -10,16 +10,17 @@ class ClassifieldType extends AbstractType
     public function buildForm(FormBuilder $builder, array $options)
     {
         $builder
+            ->add('category')
+            ->add('picture')
             ->add('title')
             ->add('description')
+            ->add('is_billable')
             ->add('currency')
             ->add('price')
             ->add('status')
             ->add('created_at')
             ->add('updated_at')
             ->add('published_at')
-            ->add('picture')
-            ->add('category')
             ->add('user')
         ;
     }
