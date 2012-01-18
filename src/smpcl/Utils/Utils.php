37557e4cff06d494ef944f2406538287b261a62e
Marian0 <marianosantafe@gmail.com>
2012-01-18 20:03:54 -0300

    Se agrego un parameters en el config que sirve para agregar parametros de la aplicacion. Se agrego la clase Utils para sluglify pero aun no se como incluirla. Se agrego un nuevo footer en el layout base

---
+++ src/smpcl/Utils/Utils.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace \Utils;
+
+class Utils {
+
+    static public function getSlug($cadena, $separador = '-') {
+
+        $slug = iconv('UTF-8', 'ASCII//TRANSLIT', $cadena);
+        $slug = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $slug);
+        $slug = strtolower(trim($slug, $separador));
+        $slug = preg_replace("/[\/_|+ -]+/", $separador, $slug);
+        return $slug;
+    }
+
+};
