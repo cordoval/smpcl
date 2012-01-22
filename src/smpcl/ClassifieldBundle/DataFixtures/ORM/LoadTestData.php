e32ccfbbec97af955c1b8ce1ec09db315abb65d0
Marian0 <marianosantafe@gmail.com>
2012-01-22 19:44:06 -0300

    Se actualizó el fichero de Fixtures para agregar automaticamente usuarios administradores marian0 y germanaz0. Tambien se agregaron algunos parametros de configuracion en security.yml para customizar la encriptación de la clave de los usuarios (Puede ser util para posteriores migraciones).

--- src/smpcl/ClassifieldBundle/DataFixtures/ORM/LoadTestData.php
+++ src/smpcl/ClassifieldBundle/DataFixtures/ORM/LoadTestData.php
@@ -2,17 +2,19 @@
 
 namespace smpcl\ClassifieldBundle\DataFixtures\ORM;
 
-
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use smpcl\ClassifieldBundle\Entity\Category;
 
-class LoadUserData implements FixtureInterface
-{
-    public function load($manager)
-    {
+use smpcl\UserBundle\Entity\User;
+use Symfony\Component\Security\Core\Role\RoleInterface;
+use Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder;
+
+class LoadUserData implements FixtureInterface {
+
+    public function load($manager) {
         //Primero cargamos Categorias
-        
-        
+
+
         $categorias = array(
             'Telefonía',
             'Informática',
@@ -34,16 +36,71 @@ class LoadUserData implements FixtureInterface
             'Varios',
             'Mascotas y Plantas',
             'Construccion',
-            'Empleos',            
+            'Empleos',
         );
-        
-        foreach($categorias as $cat) {
+
+        foreach ($categorias as $cat) {
             $c = new Category();
             $c->setTitle($cat);
             $c->setDescription('Descripcion de la categoria ' . $cat);
             $c->setTags('TAGS TAGS ' . $cat);
             $manager->persist($c);
         }
+
+
+
+        //Ahora los Usuarios
+
+
+
+        // create a user para marian0 (ADMIN)
+        $user = new User();
+        $user->setEmail('marianosantafe@gmail.com');
+        $user->setUsername('marian0');
+        $user->setEnabled(1);
+
+        // encode and set the password for the user,
+        // these settings match our config
+        //(Debe matchear con security.yml
+        $encoder = new MessageDigestPasswordEncoder('sha512', true, 10);
+        $password = $encoder->encodePassword('admin', $user->getSalt());
+        $user->setPassword($password);
+        $user->setRoles(array('ROLE_USER','ROLE_ADMIN'));
+        $manager->persist($user);
+        
+        // create a user para marian0 (ADMIN)
+        $user = new User();
+        $user->setEmail('germanazo@gmail.com');
+        $user->setUsername('germanaz0');
+        $user->setEnabled(1);
+
+        // encode and set the password for the user,
+        // these settings match our config
+        //(Debe matchear con security.yml
+        $encoder = new MessageDigestPasswordEncoder('sha512', true, 10);
+        $password = $encoder->encodePassword('admin', $user->getSalt());
+        $user->setPassword($password);
+        $user->setRoles(array('ROLE_USER','ROLE_ADMIN'));
+        $manager->persist($user);
+
+        
+        //agregamos usuarios comunes
+        $user = new User();
+        $user->setEmail('userloco@gmail.com');
+        $user->setUsername('user');
+        $user->setEnabled(1);
+
+        // encode and set the password for the user,
+        // these settings match our config
+        //(Debe matchear con security.yml
+        $encoder = new MessageDigestPasswordEncoder('sha512', true, 10);
+        $password = $encoder->encodePassword('user', $user->getSalt());
+        $user->setPassword($password);
+        $user->setRoles(array('ROLE_USER'));
+        $manager->persist($user);
+
+
         $manager->flush();
     }
+
 }
