a1146ccc2488c07d19793a468675fe7b7297a93b
Germanaz0 <germanazo@gmail.com>
2012-01-22 02:57:21 -0300

    Se deshabilito el versionable que trajo la instalacion del admin bundle. Borrar de la base de dato las tablas audit y versions

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,7 +27,7 @@ class AppKernel extends Kernel {
             new Sonata\AdminBundle\SonataAdminBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
-            new SimpleThings\EntityAudit\SimpleThingsEntityAuditBundle(),
+           
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- app/autoload.php
+++ app/autoload.php
@@ -24,7 +24,6 @@ $loader->registerNamespaces(array(
     'Sonata'     => __DIR__.'/../vendor/bundles',
     'Knp\Bundle' => __DIR__.'/../vendor/bundles',
     'Knp\Menu'   => __DIR__.'/../vendor/knp/menu/src',
-     'SimpleThings'      => __DIR__.'/../vendor/bundles',
 ));
 $loader->registerPrefixes(array(
     'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
